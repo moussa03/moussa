c4c02e1279e9e389c1f3d5bd7d3930ae2a7778fb
moussa <moussaeloifi14@gmail.com>
2022-11-13 21:18:26 +0100

    replace welcome with home page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Welcome');
+    return view('home');
 })->name('home');
 
 Route::get('/contactez_nous',[App\Http\Controllers\UserController::class,'contactez_nous']);
